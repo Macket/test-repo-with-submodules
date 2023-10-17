ca45a0ad1524790156dd6a1801e8c38db3dc5172
macket <macket24@proton.me>
2023-10-17 18:50:42 +0400

    fix: sync

--- sync.sh
+++ sync.sh
@@ -2,4 +2,4 @@
 git submodule update --remote --merge
 
 cp ./test-submodule1/test/test1.txt ./dist/copy-test1.txt
-cp ./test-submodule2/test/test.txt ./dist/copy-test2.txt
+cp ./test-submodule2/test/test2.txt ./dist/copy-test2.txt
